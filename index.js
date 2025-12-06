72ab0ce2757dfddddf565645fdfa709b588db775
Forhad Reza <forhad.bimt@gmail.com>
2025-12-06 17:45:42 +0600

    Add payment processing routes and controller for handling payments

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import userRoutes from "./routes/userRoutes.js";
 import packageRoutes from "./routes/packageRoutes.js";
 import storyRoutes from "./routes/storyRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
-// import paymentRoutes from "./routes/paymentRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
 // import adminRoutes from "./routes/adminRoutes.js";
 // import guideApplicationRoutes from "./routes/guideApplicationRoutes.js";
 
@@ -40,7 +40,7 @@ app.use("/packages", packageRoutes);
 // app.get("/random-packages", getRandomPackages); // Backward compatibility - root level
 app.use("/stories", storyRoutes);
 app.use("/bookings", bookingRoutes);
-// app.use("/", paymentRoutes);
+app.use("/", paymentRoutes);
 // app.use("/admin", adminRoutes);
 // app.use("/guide-applications", guideApplicationRoutes);
 
